03646ae390f9e6edb296fb5af5103acff829ce69
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-09-02 12:53:49 +0530

    add fav icons

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -5,6 +5,8 @@ import Categories from "../views/Categories"
 import Meals from "../views/Meals"
 import MealDetails from "../views/MealDetails"
 import { Colors } from "../constants/Colors"
+import { TouchableNativeFeedback } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
 
 export default function Navigator() {
   const Stack = createNativeStackNavigator()
@@ -39,7 +41,16 @@ export default function Navigator() {
           name="meal-details"
           options={({ route }) => ({
             title: route.params.title,
-            headerStyle: { backgroundColor: route.params.color }
+            headerStyle: { backgroundColor: route.params.color },
+            headerRight: () => (
+              <TouchableNativeFeedback>
+                <Ionicons
+                  name="ios-star"
+                  onPress={() => {}}
+                  style={{ fontSize: 25, color: "#f1f1f1" }}
+                />
+              </TouchableNativeFeedback>
+            )
           })}
           component={MealDetails}
         />
