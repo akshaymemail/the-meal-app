602b84501e0a4c3f2921bd51961c1d0e2da15b64
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-09-02 18:17:43 +0530

    complete filter page

--- views/Filters/index.js
+++ views/Filters/index.js
@@ -1,10 +1,51 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from "react"
+import { View, Text, StyleSheet, Switch } from "react-native"
+import { Colors } from "../../constants/Colors"
+
+const FilterSwitch = ({ label, state, onChange }) => (
+  <View style={styles.filterContainer}>
+    <Text>{label}</Text>
+    <Switch
+      trackColor={{ true: Colors.primaryColor }}
+      thumbColor={Colors.primaryColor}
+      value={state}
+      onValueChange={() => onChange(!state)}
+    />
+  </View>
+)
 
 export default function Filters() {
+  const [isGlutenFree, setIsGlutenFree] = useState(false)
+  const [isVegetarian, setIsVegetarian] = useState(false)
+  const [isNonVegetarian, setIsNonVegetarian] = useState(true)
+  const [isVegan, setIsVegan] = useState(false)
   return (
     <View style={styles.screen}>
-      <Text>Filters Screen</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      {[
+        {
+          label: "Gluten-Free",
+          state: isGlutenFree,
+          onChange: setIsGlutenFree
+        },
+        { label: "Vegetarian", state: isVegetarian, onChange: setIsVegetarian },
+        {
+          label: "NonVegetarian",
+          state: isNonVegetarian,
+          onChange: setIsNonVegetarian
+        },
+        {
+          label: "Is Vegan",
+          state: isVegan,
+          onChange: setIsVegan
+        }
+      ].map((rule) => (
+        <FilterSwitch
+          label={rule.label}
+          state={rule.state}
+          onChange={rule.onChange}
+        />
+      ))}
     </View>
   )
 }
@@ -12,7 +53,19 @@ export default function Filters() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: "center"
   },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center"
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 20
+  }
 })
